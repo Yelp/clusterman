a9002c820f218b34f31c3fc72dd20bebbdc068e0
David Morrison <drmorr@yelp.com>
2019-07-15 16:56:07 -0700

    fix excluded resources issue

--- tests/autoscaler/config_test.py
+++ tests/autoscaler/config_test.py
@@ -1,5 +1,6 @@
 import staticconf.testing
 
+from clusterman.autoscaler.config import AutoscalingConfig
 from clusterman.autoscaler.config import get_autoscaling_config
 
 
@@ -7,13 +8,14 @@ def test_get_autoscaling_config():
     default_autoscaling_values = {
         'setpoint': 0.7,
         'setpoint_margin': 0.1,
+        'excluded_resources': ['gpus']
     }
     pool_autoscaling_values = {
         'setpoint': 0.8,
+        'excluded_resources': ['cpus']
     }
     with staticconf.testing.MockConfiguration({'autoscaling': default_autoscaling_values}), \
             staticconf.testing.MockConfiguration({'autoscaling': pool_autoscaling_values}, namespace='pool_namespace'):
         autoscaling_config = get_autoscaling_config('pool_namespace')
 
-        assert autoscaling_config.setpoint == 0.8
-        assert autoscaling_config.setpoint_margin == 0.1
+        assert autoscaling_config == AutoscalingConfig(['cpus'], 0.8, 0.1)
