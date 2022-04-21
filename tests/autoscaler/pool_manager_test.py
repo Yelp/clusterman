57178f707ea2b0bbdc838ad8ecdd7cbfe2b74ab1
ilkinmammadzada <ilkinmammadzada@yelp.com>
2022-04-21 22:53:46 +0100

    calculation of the instance uptime was changed

--- tests/autoscaler/pool_manager_test.py
+++ tests/autoscaler/pool_manager_test.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+import arrow
 import mock
 import pytest
 import staticconf
@@ -57,7 +58,7 @@ def _make_metadata(
             is_stale=is_stale,
             market="market-1",
             state="running",
-            uptime=uptime,
+            uptime=(arrow.now().shift(seconds=uptime) - arrow.now()),
             weight=weight,
         ),
     )
