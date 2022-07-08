6c89e2a41b2df5939860947ed18a6f6fa4c9fb8d
Ilkin Mammadzada <39277455+ilkinmammadzada@users.noreply.github.com>
2022-07-08 16:34:07 +0100

    avoid freezing orphaned instances (#172)

--- clusterman/autoscaler/pool_manager.py
+++ clusterman/autoscaler/pool_manager.py
@@ -439,9 +439,12 @@ class PoolManager:
                     )
                     continue
 
-            logger.info(f"freezing {instance_id} for termination")
-            if not dry_run:
-                self.cluster_connector.freeze_agent(node_metadata.agent.agent_id)
+            # Orphaned instances don't have agent information
+            if node_metadata.agent.state != AgentState.ORPHANED:
+                logger.info(f"freezing {node_metadata.agent.agent_id} for termination")
+                if not dry_run:
+                    self.cluster_connector.freeze_agent(node_metadata.agent.agent_id)
+
             logger.info(f"marking {instance_id} for termination")
             marked_nodes[group_id].append(node_metadata)
             rem_group_capacities[group_id] -= instance_weight
