066b581a411604e3fa53de2b3e89cd88092e993d
Akshay Sharma <akshaysha@yelp.com>
2022-08-08 07:52:18 -0700

    CLUSTERMAN-582, fix nits

--- clusterman/kubernetes/kubernetes_cluster_connector.py
+++ clusterman/kubernetes/kubernetes_cluster_connector.py
@@ -182,9 +182,9 @@ class KubernetesClusterConnector(ClusterConnector):
         return True
 
     def _get_nodes_by_ip(self) -> Mapping[str, KubernetesNode]:
-        use_different_label_for_nodes = self.pool_config.read_string("use_different_label_for_nodes", False)
+        use_different_label_for_nodes = self.pool_config.read_bool("use_different_label_for_nodes", False)
 
-        if use_different_label_for_nodes: 
+        if use_different_label_for_nodes:
             node_label_selector = self.pool_config.read_string("node_label_key", default="clusterman.com/pool")
         else:
             node_label_selector = self.pool_config.read_string("pool_label_key", default="clusterman.com/pool")
