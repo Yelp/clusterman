e22f998c7bc5a7a080ba16eefdf9e61d0436313f
Akshay Sharma (GitHub) <akshay.sharma09695@gmail.com>
2022-08-26 15:56:15 +0100

    Fix tests

--- tests/kubernetes/kubernetes_cluster_connector_test.py
+++ tests/kubernetes/kubernetes_cluster_connector_test.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from copy import deepcopy
+
 import mock
 import pytest
 from kubernetes.client import V1Container
