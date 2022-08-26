c05c22861fe3717df04b02fb4874a0ff5c5f0eb1
Akshay Sharma (GitHub) <akshay.sharma09695@gmail.com>
2022-08-26 16:16:44 +0100

    Use function to get pods by label when feature flag is used

--- clusterman/kubernetes/kubernetes_cluster_connector.py
+++ clusterman/kubernetes/kubernetes_cluster_connector.py
@@ -78,7 +78,7 @@ class KubernetesClusterConnector(ClusterConnector):
             self._pods_by_ip,
             self._unschedulable_pods,
             self._excluded_pods_by_ip,
-        ) = self._get_pods_info()
+        ) = self._get_pods_info_with_label() if self.pool_config.read_bool("use_labels_for_pods", default=False) else self._get_pods_info()
 
     def get_num_removed_nodes_before_last_reload(self) -> int:
         previous_nodes = self._prev_nodes_by_ip
