64cd37c1ac0a5f5b53a5a5371b14eac3b095e4e8
Luis Pérez <luis.i.perez.0@gmail.com>
2022-09-14 17:49:31 -0400

    Fix awscli conflict (#194)
    We install a forked aws-cli internally, and that conflicts with the
    upstream awscli
    
    Co-authored-by: tzhu <49459518+tzhu-yelp@users.noreply.github.com>

--- package/itest/ubuntu.sh
+++ package/itest/ubuntu.sh
@@ -18,6 +18,7 @@ highlight_exec() {
 
 PACKAGE_NAME="$1"
 PACKAGE_VERSION="$2"
+PAASTA_ENV="$3"
 
 # This will get DISTRIB_CODENAME
 source /etc/lsb-release
@@ -31,15 +32,22 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 apt-get update && apt-get install -y software-properties-common
 # we really only need this externally, but we use a python not included
 # by ubuntu - so add the deadsnakes ppa to bring that in
-if [ "${DISTRIB_CODENAME}" = "jammy" ]; then
+if [ "${PAASTA_ENV}" != "YELP" ]; then
     add-apt-repository ppa:deadsnakes/ppa
 fi
 # our debian/control will already install py3.7, but we want to install it ahead of time so that
-# we can also get the right pip version installed as well
-apt-get install -y --force-yes python3.7 python3-pip
-dpkg -i /dist/${DISTRIB_CODENAME}/clusterman_${PACKAGE_VERSION}_amd64.deb || true
-# dpkg -i will have left some dependencies uninstalled, so this will install them
-apt-get install -y --force-yes --fix-broken
+# we can also get the right pip version installed as well.
+apt-get install -y --force-yes python3.7 python3-pip python3-yaml
+# Install package directly with any needed dependencies
+
+# we also install python3-distutils here to avoid issues on newer ubuntus
+# where disutils isn't included with python (and even though clusterman depends on it, the right
+# version isn't installed in this itest container)
+if  [ "${DISTRIB_CODENAME}" != "xenial" ]; then
+apt-get install -y --force-yes python3.7-distutils
+fi
+
+apt-get install -y --force-yes ./dist/${DISTRIB_CODENAME}/clusterman_${PACKAGE_VERSION}_amd64.deb
 
 # Sometimes our acceptance tests run in parallel on the same box, so we need to use different CIDR ranges
 if [ "${DISTRIB_CODENAME}" = "xenial" ]; then
@@ -49,7 +57,7 @@ else
 fi
 
 export ACCEPTANCE_ROOT=/itest
-python3.7 -m pip install boto3 simplejson
+python3.7 -m pip install boto3 simplejson pyyaml
 python3.7 /itest/run_instance.py \
     http://moto-ec2:5000/ \
     http://moto-s3:5000/ \
